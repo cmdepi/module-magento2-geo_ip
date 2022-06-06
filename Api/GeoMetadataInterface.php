98318b9f09e4c263b91bce9acc08adb3bc050b02
Cristian Marcelo de Picciotto <cmdepicciotto@gmail.com>
2022-06-06 07:29:31 -0300

    IN-235 | IN-249 | Init geo metadata interface

---
+++ Api/GeoMetadataInterface.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ * @description Geo metadata interface
+ *
+ * @author Bina Commerce      <https://www.binacommerce.com>
+ * @author C. M. de Picciotto <cmdepicciotto@binacommerce.com>
+ *
+ */
+namespace Bina\GeoIp\Api;
+
+interface GeoMetadataInterface
+{
+    /**
+     *
+     * @const GEO_COUNTRY_HEADER
+     *
+     */
+    const GEO_COUNTRY_HEADER = 'CF-IPCountry';
+}
